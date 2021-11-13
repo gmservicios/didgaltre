da9ea242b5f975125c51093271f3a4f77dfbc25c
gmservicios <75394149+gmservicios@users.noreply.github.com>
2021-11-12 20:39:52 -0600

    Delete sale_custom directory

--- sale_custom/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import product
-from . import res_users
-from . import stock
-from . import res_partner
-from . import account_payment_term
-from . import sale
-from . import account_invoice
--- sale_custom/models/account_payment_term.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api, _
-
-class PaymentTerm(models.Model):
-	_inherit = 'account.payment.term'
-
-	payment_method = fields.Selection(selection=[('counted','Counted'),('credit','Credit')], string="Method payment", required=True)
